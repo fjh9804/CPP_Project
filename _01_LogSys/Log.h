e3e68ac42b75b2a870cdf720cc0b4e205165391c
Frank <fjh9804@163.com>
2025-12-13 11:03:54 +0800

    添加第1个项目-日志系统

---
+++ _01_LogSys/Log.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+//定义日志等级
+enum class LogLevel {
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR
+};
+
+//定义日志类
+class Logger {
+private:
+	LogLevel logLv;
+	std::vector<std::shared_ptr<>>
+};
---
+++ _01_LogSys/main.cpp
@@ -0,0 +1,6 @@
+#include "Log.h"
+
+int main(void) {
+
+	return 0;
+}
